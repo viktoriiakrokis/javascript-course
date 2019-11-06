747d9ab815ca9307a61ffb93b4314fb2582df499
viktoriiakrokis <vika.krokis063@gmail.com>
2019-11-06 15:50:52 +0200

    finnish of loan calculator

--- loan-calculator/built-theUl.js
+++ loan-calculator/built-theUl.js
@@ -1,15 +1,25 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e){
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+  
+  // Show loader
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 // Calculate Results
-function calculateResults(e){
+function calculateResults(){
   console.log('Calculating...');
   // UI Vars
   const amount = document.getElementById('amount');
   const interest = document.getElementById('interest');
   const years = document.getElementById('years');
-  const monthlyPayment = document.getElementById('amonthly-payment');
-  const totalPayment= document.getElementById('total-payment');
+  const monthlyPayment = document.getElementById('monthly-payment');
+  const totalPayment = document.getElementById('total-payment');
   const totalInterest = document.getElementById('total-interest');
 
   const principal = parseFloat(amount.value);
@@ -20,28 +30,39 @@ function calculateResults(e){
   const x = Math.pow(1 + calculatedInterest, calculatedPayments);
   const monthly = (principal*x*calculatedInterest)/(x-1);
 
-  if (isFinite(monthly)) {
+  if(isFinite(monthly)) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
-    totalInterest.value =((monthly * calculatedPayments) - principal).toFixed(2);
+    totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+
+    // Show results
+    document.getElementById('results').style.display = 'block';
+
+    // Hide loader
+    document.getElementById('loading').style.display = 'none';
+
   } else {
     showError('Please check your numbers');
   }
-
-  e.preventDefault();
 }
 
-// Show Error 
-function showError(error) {
+// Show Error
+function showError(error){
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+  
+  // Hide loader
+  document.getElementById('loading').style.display = 'none';
+
   // Create a div
   const errorDiv = document.createElement('div');
 
-  //Get elements
+  // Get elements
   const card = document.querySelector('.card');
   const heading = document.querySelector('.heading');
 
   // Add class
-  errorDiv.className = 'alert alert danger';
+  errorDiv.className = 'alert alert-danger';
 
   // Create text node and append to div
   errorDiv.appendChild(document.createTextNode(error));
@@ -53,8 +74,7 @@ function showError(error) {
   setTimeout(clearError, 3000);
 }
 
-// Clear Error
-
-function clearError() {
+// Clear error
+function clearError(){
   document.querySelector('.alert').remove();
 }

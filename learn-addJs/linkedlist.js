5bf9503f5dc4110096010dc0fbede379a235a58e
viktoriiakrokis <vika.krokis063@gmail.com>
2019-11-23 16:44:45 +0200

    add

--- learn-addJs/linkedlist.js
+++ learn-addJs/linkedlist.js
@@ -14,28 +14,74 @@ class LinkedList {
   }
 
 
-// Insert first node
-  insertFirst(data) {
-    this.head = new Node(data, this.head);
+  // Insert first node
+    insertFirst(data) {
+      this.head = new Node(data, this.head);
+    }
+
+  // Insert last node
+    insertLast(data) {
+      let node = new Node(data);
+      let current;
+    
+      // If empty, make head
+      if(!this.head) {
+        this.head = node;
+      } else {
+        current = this.head;
+
+        while(current.next) {
+          current = current.next;
+        }
+
+        current.next = node;
+      }
+
+      this.size++;
+    }
+
+  // Insert in(using index) index
+  insertAt(data, index) {
+    // If index is out of range 
+    if(index > 0 && index > this.size){
+      return;
+    }
+
+    // If first index
+    if(index===0) {
+      this.head = new Node(data, this.head);
+      return;
+    }
+
+    const node = new Node(data);
+    let current, previous;
+
+    // Set current to first
+    current = this.head;
+    let count = 0;
+
+    while(count < index) {
+      previous = current; // Node before index
+      count++;
+      current = current.next; // Node after index
+    }
+
+    node.next = current;
+    previous.next = node;
+
+    this.size++;
   }
 
-// Insert last node
+  //  Get an index
 
 
-// Insert in index
+  // Remove at index
 
 
-//  Get an index
+  // Clear the list
 
 
-// Remove at index
-
-
-// Clear the list
-
-
-// Print list data
-
+  // Print list data
   printListData () {
     let current = this.head;
 
@@ -47,10 +93,14 @@ class LinkedList {
 }
 
 
-const ll = new LinkedList();
+  const ll = new LinkedList();
 
-ll.insertFirst(100);
-ll.insertFirst(200);
+  ll.insertFirst(100);
+  ll.insertFirst(200);
+  ll.insertFirst(300);
+  ll.insertLast(400);
 
-console.log(ll);
+  ll.insertAt(500, 2);
+
+  ll.printListData();
 

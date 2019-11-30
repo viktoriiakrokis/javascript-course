d16769a7016bd53c1c7bd0749818f8edfcc1f8e4
viktoriiakrokis <vika.krokis063@gmail.com>
2019-11-30 18:09:16 +0200

    Search in Rotated Sorted Array

---
+++ algorithms/search-In-Array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+  for (let i=0; i<nums.length; i++) {
+      if(target == nums[i]){
+          return i;
+          break;
+      }
+      
+      
+  }
+  return -1;
+};

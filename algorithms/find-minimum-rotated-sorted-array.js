a153635ce271fa0a1e19c91a4c8290f8f6d168a7
viktoriiakrokis <vika.krokis063@gmail.com>
2019-12-19 20:25:19 +0200

    Find Minimum in Rotated Sorted Array

---
+++ algorithms/find-minimum-rotated-sorted-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    let minNum = 1000;
+    
+    for(let i=0; i<nums.length; i++){
+        if(nums[i] < minNum){
+            minNum = nums[i];
+        } 
+        
+    }
+     return minNum;    
+};

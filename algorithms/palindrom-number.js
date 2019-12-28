3fa957d33bdb635305bfec437deae1a9bfba2e5c
viktoriiakrokis <vika.krokis063@gmail.com>
2019-12-28 20:38:50 +0200

    move algorithms to separate repository

--- algorithms/palindrom-number.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {number} x
- * @return {boolean}
- */
-var isPalindrome = function(x) {
-    
-    var newString = x.toString();
-    return newString.split('').every((element, index, array) => element === array[array.length-index-1]);
-};

b7a95f39779f4e309b557aa2a2e1488ad6f6575e
viktoriiakrokis <vika.krokis063@gmail.com>
2020-01-03 11:08:21 +0200

    The onFulfilled and onRejected Functions

---
+++ codeacademy/JS-promises/onFulfille-onRejected-functions.js
@@ -0,0 +1,50 @@
+// handle a “successful” promise, or a promise that resolved,  invoke .then()
+
+const prom = new Promise((resolve, reject) => {
+  resolve('Yay!');
+});
+
+const handleSuccess = (resolvedValue) => {
+  console.log(resolvedValue);
+};
+
+prom.then(handleSuccess); // Prints: 'Yay!'
+
+// both an onFulfilled and onRejected callback to .then()
+
+let prom = new Promise((resolve, reject) => {
+  let num = Math.random();
+  if (num < .5 ){
+    resolve('Yay!');
+  } else {
+    reject('Ohhh noooo!');
+  }
+});
+
+const handleSuccess = (resolvedValue) => {
+  console.log(resolvedValue);
+};
+
+const handleFailure = (rejectionReason) => {
+  console.log(rejectionReason);
+};
+
+prom.then(handleSuccess, handleFailure);
+
+// exercise
+
+const {checkInventory} = require('./library.js');
+
+const order = [['sunglasses', 1], ['bags', 2]];
+
+// Write your code below:
+const handleSuccess = (resolvedValue) => {
+    console.log(resolvedValue);
+};
+
+const handleFailure = (rejectReason) => {
+    console.log(rejectReason);
+};
+
+checkInventory(order)
+    .then(handleSuccess, handleFailure);

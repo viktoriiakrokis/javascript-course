c04a08578847cbb2a3d01ad5d9a95f3a68c9ce66
viktoriiakrokis <vika.krokis063@gmail.com>
2020-01-05 18:00:43 +0200

    Chaining Multiple Promises

---
+++ codeacademy/JS-promises/chaining-multiple-promises.js
@@ -0,0 +1,34 @@
+// chaining promises together is composition.
+
+firstPromiseFunction()
+.then((firstResolveVal) => {
+  return secondPromiseFunction(firstResolveVal);
+})
+.then((secondResolveVal) => {
+  console.log(secondResolveVal);
+});
+
+// exercise
+
+const {checkInventory, processPayment, shipOrder} = require('./library.js');
+
+const order = {
+  items: [['sunglasses', 1], ['bags', 2]],
+  giftcardBalance: 79.82
+};
+
+checkInventory(order)
+.then((resolvedValueArray) => {
+  // Write the correct return statement here: 
+ return processPayment(resolvedValueArray);
+})
+.then((resolvedValueArray) => {
+  // Write the correct return statement here:
+  return shipOrder(resolvedValueArray);
+})
+.then((successMessage) => {
+  console.log(successMessage);
+})
+.catch((errorMessage) => {
+  console.log(errorMessage);
+});

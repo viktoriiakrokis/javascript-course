a15b68f447d7ce7c944cbc03645594cb3506dc43
viktoriiakrokis <vika.krokis063@gmail.com>
2020-01-07 09:51:51 +0200

    Basic of ASYNC AWAIT

---
+++ codeacademy/async-await/async-keyword.js
@@ -0,0 +1,54 @@
+// buildind asynchronous logic with async
+async function myFunc() {
+  // Function body here
+};
+
+myFunc();
+
+// create async function expressions
+const myFunc = async () => {
+  // Function body here
+};
+
+myFunc();
+
+
+async function fivePromise() { 
+  return 5;
+}
+
+fivePromise()
+.then(resolvedValue => {
+    console.log(resolvedValue);
+  })  // Prints 5
+
+
+// exercise
+function withConstructor(num){
+  return new Promise((resolve, reject) => {
+    if (num === 0){
+      resolve('zero');
+    } else {
+      resolve('not zero');
+    }
+  })
+}
+
+withConstructor(0)
+  .then((resolveValue) => {
+  console.log(` withConstructor(0) returned a promise which resolved to: ${resolveValue}.`);
+})
+
+// Write your code below:
+async function withAsync(num){
+  if (num === 0){
+      return 'zero';
+    } else {
+      return 'not zero';
+    }
+}
+
+withAsync(100)
+  .then((resolveValue) => {
+  console.log(` withAsync(100) returned a promise which resolved to: ${resolveValue}.`);
+})
